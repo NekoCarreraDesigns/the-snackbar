5696484d1ad05bb3775cb113644966bfc0eef7cf
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-09 16:01:15 -0600

    created flavor component

--- the-snackbar/src/app/app.module.ts
+++ the-snackbar/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlavorsComponent } from './flavors/flavors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlavorsComponent
   ],
   imports: [
     BrowserModule,
---
+++ the-snackbar/src/app/flavors/flavors.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core'; 
+
+@Component({
+  selector: 'app-flavors',
+  templateUrl: './flavors.component.html',
+  styleUrls: ['./flavors.component.css']
+})
+export class FlavorsComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
