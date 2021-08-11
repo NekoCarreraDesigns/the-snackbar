983a97713199679491234f8a323c80ad551bd834
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-11 11:37:56 -0600

    added mock-flavors.ts, and added flavors array

---
+++ the-snackbar/src/app/mock-flavors.ts
@@ -0,0 +1,14 @@
+import { Flavors } from './flavors';
+
+export const FLAVORS: Flavors [] = [
+    { type: "cake", flavor: "Lucky Charms", quantity: 36, availability: "In Stock", price: 15},
+    { type: "cake", flavor: "Chocolate Overlord", quantity: 36, availability: "In Stock", price: 15},
+    { type: "cake", flavor: "Sunrise Daydreams", quantity: 36, availability: "Unavailable", price: 15},
+    { type: "cake", flavor: "Hummingbird", quantity: 36, availability: "In Stock", price: 15},
+    { type: "ice cream", flavor: "Lucky Charms", quantity: 16 + 32, availability: "In Stock", price: 8 + 14},
+    { type: "ice cream", flavor: "Marshmallow Fudge Surprise", quantity: 16 + 32, availability: "In Stock", price: 8 + 14},
+    { type: "ice cream", flavor: "Tropical Punch", quantity: 16 + 32, availability: "Unavailable", price: 8 + 14},
+    { type: "chocolate", flavor: "Raspberry Ganache", quantity: 36, availability: "In Stock", price: 24},
+    { type: "chocolate", flavor: "Tuxedo Ganache", quantity: 36, availability: "Unavailable", price: 24},
+    { type: "chocolate", flavor: "Dark and Dreamy", quantity: 36, availability: "In Stock", price: 24}
+]
