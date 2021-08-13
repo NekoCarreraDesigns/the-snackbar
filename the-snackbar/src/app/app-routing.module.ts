3c07e9c42e8b6aa5612956bac459ff0c065d685d
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-13 14:28:26 -0600

    added welcome component html, and the route for the home page

--- the-snackbar/src/app/app-routing.module.ts
+++ the-snackbar/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlavorsComponent } from './flavors/flavors.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'flavors', component: FlavorsComponent},
+  {path: 'welcome', component: WelcomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
