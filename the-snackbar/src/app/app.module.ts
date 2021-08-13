11599e14d6fceaadb4cae52338a62149aeeb8fc2
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-13 12:13:16 -0600

    added flavor service, and created home page

--- the-snackbar/src/app/app.module.ts
+++ the-snackbar/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlavorsComponent } from './flavors/flavors.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FlavorsComponent
+    FlavorsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
