2a085c9c04cb55cab9b6e6f41759bb55b7ca031b
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-18 17:05:59 -0600

    added route to log-in

--- the-snackbar/src/app/app-routing.module.ts
+++ the-snackbar/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FlavorsComponent } from './flavors/flavors.component';
+import { LogInComponent } from './log-in/log-in.component';
 import { PurchaseComponent } from './purchase/purchase.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path: '', redirectTo: '/welcome', pathMatch: 'full'},
   {path: 'flavors', component: FlavorsComponent},
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'purchase', component: PurchaseComponent}
+  {path: 'purchase', component: PurchaseComponent},
+  {path: 'log-in', component: LogInComponent}
   
 ];
 
