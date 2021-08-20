7019eb04928b195ea845447d58784776d8691acb
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-20 10:28:40 -0600

    added route for snack-shack

--- the-snackbar/src/app/app-routing.module.ts
+++ the-snackbar/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FlavorsComponent } from './flavors/flavors.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { PurchaseComponent } from './purchase/purchase.component';
+import { SnackShackComponent } from './snack-shack/snack-shack.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
@@ -10,7 +11,8 @@ const routes: Routes = [
   {path: 'flavors', component: FlavorsComponent},
   {path: 'welcome', component: WelcomeComponent},
   {path: 'purchase', component: PurchaseComponent},
-  {path: 'log-in', component: LogInComponent}
+  {path: 'log-in', component: LogInComponent},
+  {path: 'snack-shack', component: SnackShackComponent}
   
 ];
 
